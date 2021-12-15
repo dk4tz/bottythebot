5f96278804b64dc7d8a0d9b8b1b5f0228ad0172c
David Katz <katz@Davids-MacBook-Pro.local>
2021-12-14 22:57:23 -0500

    fix spacing issue

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+	console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.login(token);
