501fe45f8c94394d4ed5254a6e3155459a99010d
David Katz <katz@Davids-MacBook-Pro.local>
2021-12-14 22:56:39 -0500

    boilerplate deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,3 +2,16 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+    new SlashCommandBuilder().setName('ping').setDescription('replies w pong >_<'),
+    new SlashCommandBuilder().setName('server').setDescription('replies w server info O_o'),
+    new SlashCommandBuilder().setName('user').setDescription('replies w user info o_O'),
+]
+    .map(command => command.toJSON);
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Success registering commandz in ze server'))
+    .catch(console.error);
